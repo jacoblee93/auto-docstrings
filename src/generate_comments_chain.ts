36053a717bf3bf021fd3f5484f945a8af6e6c6df
jacoblee93 <jacoblee93@gmail.com>
2023-08-20 22:32:18 -0700

    Add README

--- src/generate_comments_chain.ts
+++ src/generate_comments_chain.ts
@@ -2,7 +2,11 @@ import { z } from "zod";
 import { zodToJsonSchema } from "zod-to-json-schema";
 import weaviate from "weaviate-ts-client";
 
-import { ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate } from "langchain/prompts";
+import {
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  SystemMessagePromptTemplate,
+} from "langchain/prompts";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 
 import { JsonOutputFunctionsParser } from "langchain/output_parsers";
@@ -10,24 +14,34 @@ import { createRetrieverTool } from "langchain/agents/toolkits";
 import { WeaviateStore } from "langchain/vectorstores/weaviate";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { OpenAIAgentTokenBufferMemory } from "langchain/agents/toolkits";
-import { RunnablePassthrough, RunnableSequence } from "langchain/schema/runnable";
+import {
+  RunnablePassthrough,
+  RunnableSequence,
+} from "langchain/schema/runnable";
 import { initializeAgentExecutorWithOptions } from "langchain/agents";
 
-import { COMMENT_GENERATION_HUMAN_TEMPLATE, COMMENT_GENERATION_SYSTEM_TEMPLATE, RESEARCH_AGENT_PREFIX } from "./prompts.js";
+import {
+  COMMENT_GENERATION_HUMAN_TEMPLATE,
+  COMMENT_GENERATION_SYSTEM_TEMPLATE,
+  RESEARCH_AGENT_PREFIX,
+} from "./prompts.js";
 
 // Something wrong with the weaviate-ts-client types, so we need to disable
 const client = (weaviate as any).client({
   scheme: process.env.WEAVIATE_SCHEME || "https",
   host: process.env.WEAVIATE_HOST || "localhost",
   apiKey: new (weaviate as any).ApiKey(
-    process.env.WEAVIATE_API_KEY || "default"
+    process.env.WEAVIATE_API_KEY || "default",
   ),
 });
 
-const vectorStore = await WeaviateStore.fromExistingIndex(new OpenAIEmbeddings(), {
-  indexName: "LangChain_idx",
-  client
-});
+const vectorStore = await WeaviateStore.fromExistingIndex(
+  new OpenAIEmbeddings(),
+  {
+    indexName: process.env.WEAVIATE_INDEX_NAME!,
+    client,
+  },
+);
 
 const tool = createRetrieverTool(vectorStore.asRetriever(), {
   name: "search_langchain_knowledge",
@@ -42,55 +56,79 @@ export async function createGenerateCommentsChain(): Promise<RunnableSequence> {
   });
 
   // Initialize outside of the function scope to reuse documents and context retrieved from
-  // processing prior files. Move into the function to save tokens.
-  const researchAgentExecutor = await initializeAgentExecutorWithOptions([tool], new ChatOpenAI({
-    // modelName: "gpt-3.5-turbo-16k",
-    modelName: "gpt-4",
-    temperature: 0.1,
-  }), {
-    agentType: "openai-functions",
-    memory,
-    returnIntermediateSteps: true,
-    agentArgs: {
-      prefix: RESEARCH_AGENT_PREFIX,
+  // processing prior files. Move into the function to save tokens and reduce potential distractions
+  // to the LLM.
+  const researchAgentExecutor = await initializeAgentExecutorWithOptions(
+    [tool],
+    new ChatOpenAI({
+      // modelName: "gpt-3.5-turbo-16k",
+      modelName: "gpt-4",
+      temperature: 0.1,
+    }),
+    {
+      agentType: "openai-functions",
+      memory,
+      returnIntermediateSteps: true,
+      agentArgs: {
+        prefix: RESEARCH_AGENT_PREFIX,
+      },
     },
-  });
+  );
 
-
-  const prompt = ChatPromptTemplate.fromPromptMessages<{input: string, context: string}>([
-    SystemMessagePromptTemplate.fromTemplate(COMMENT_GENERATION_SYSTEM_TEMPLATE),
+  const prompt = ChatPromptTemplate.fromPromptMessages<{
+    input: string;
+    context: string;
+  }>([
+    SystemMessagePromptTemplate.fromTemplate(
+      COMMENT_GENERATION_SYSTEM_TEMPLATE,
+    ),
     HumanMessagePromptTemplate.fromTemplate(COMMENT_GENERATION_HUMAN_TEMPLATE),
   ]);
 
   const schema = z.object({
-    ts_doc_comments: z.array(z.object({
-      text: z.string().describe("The full TSDoc text, including newlines"),
-      name: z.string().describe("The method or class name"),
-      type: z.enum(["method", "class", "type", "interface", "function"]),
-      params: z.array(z.object({
-        name: z.string().describe("The name of the param"),
-        description: z.string().describe("A description of the parameter"),
-      })).describe("For methods, a list of parameters the method takes. Should contain all information to render as an @param declaration. Should be empty for non-methods"),
-      returns: z.optional(z.string()).describe("For methods, information on what the method returns")
-    }))
+    ts_doc_comments: z.array(
+      z.object({
+        text: z.string().describe("The full TSDoc text, including newlines"),
+        name: z.string().describe("The method or class name"),
+        type: z.enum(["method", "class", "type", "interface", "function"]),
+        params: z
+          .array(
+            z.object({
+              name: z.string().describe("The name of the param"),
+              description: z
+                .string()
+                .describe("A description of the parameter"),
+            }),
+          )
+          .describe(
+            "For methods, a list of parameters the method takes. Should contain all information to render as an @param declaration. Should be empty for non-methods",
+          ),
+        returns: z
+          .optional(z.string())
+          .describe("For methods, information on what the method returns"),
+      }),
+    ),
   });
 
   const functionCallingModel = new ChatOpenAI({
     modelName: "gpt-4",
     temperature: 0.1,
   }).bind({
-    functions: [{
-      name: "comment_inserter",
-      description: "Inserts TSDoc comments into code based on input arguments",
-      parameters: zodToJsonSchema(schema),
-    }],
-    function_call: { name: "comment_inserter" }
+    functions: [
+      {
+        name: "comment_inserter",
+        description:
+          "Inserts TSDoc comments into code based on input arguments",
+        parameters: zodToJsonSchema(schema),
+      },
+    ],
+    function_call: { name: "comment_inserter" },
   });
 
   return RunnableSequence.from([
     {
       original_input: new RunnablePassthrough(),
-      retrievalResult: researchAgentExecutor
+      retrievalResult: researchAgentExecutor,
     },
     {
       input: ({ original_input }) => original_input.input,
@@ -98,6 +136,6 @@ export async function createGenerateCommentsChain(): Promise<RunnableSequence> {
     },
     prompt,
     functionCallingModel,
-    new JsonOutputFunctionsParser()
+    new JsonOutputFunctionsParser(),
   ]);
 }
--- src/index.ts
+++ src/index.ts
@@ -7,55 +7,75 @@ import { createGenerateCommentsChain } from "./generate_comments_chain.js";
 const directoryPath = process.argv[2];
 
 if (!directoryPath) {
-  throw new Error("No directory path provided.")
+  throw new Error("No directory path provided.");
 }
 
-const walk = (dirPath) => {
+const walk = (dirPath: string): any[] => {
   if (fs.statSync(dirPath).isFile()) {
     return [dirPath];
   }
-  const entries = fs.readdirSync(dirPath, {withFileTypes: true});
-  return entries.map((entry) => {
-    const childPath = path.join(dirPath, entry.name)
-    return entry.isDirectory() ? walk(childPath) : childPath
-  })
+  const entries = fs.readdirSync(dirPath, { withFileTypes: true });
+  return entries.map((entry: fs.Dirent) => {
+    const childPath = path.join(dirPath, entry.name);
+    return entry.isDirectory() ? walk(childPath) : childPath;
+  });
 };
 
-const filePaths = (await walk(directoryPath)).flat(Number.POSITIVE_INFINITY).filter((filePath) => {
-  if (!filePath.endsWith(".ts") || filePath.endsWith(".test.ts")) {
-    return false;
-  }
-  const sourceNode = ts.createSourceFile("dummy.ts", fs.readFileSync(filePath).toString(), ts.ScriptTarget.Latest, true);
-  const relevantNode = findMatchingUncommentedDeclarationNode(sourceNode);
-  if (!relevantNode) {
-    console.log("Skipping file", filePath);
-  }
-  return !!relevantNode;
-});
+const filePaths = (await walk(directoryPath))
+  .flat(Number.POSITIVE_INFINITY)
+  .filter((filePath) => {
+    if (!filePath.endsWith(".ts") || filePath.endsWith(".test.ts")) {
+      return false;
+    }
+    const sourceNode = ts.createSourceFile(
+      "dummy.ts",
+      fs.readFileSync(filePath).toString(),
+      ts.ScriptTarget.Latest,
+      true,
+    );
+    const relevantNode = findMatchingUncommentedDeclarationNode(sourceNode);
+    if (!relevantNode) {
+      console.log("Skipping file", filePath);
+    }
+    return !!relevantNode;
+  });
+
+type DeclarationNodeType =
+  | ts.MethodDeclaration
+  | ts.ClassDeclaration
+  | ts.InterfaceDeclaration
+  | ts.TypeAliasDeclaration
+  | ts.FunctionDeclaration;
 
 function findMatchingUncommentedDeclarationNode(
   rootNode: ts.SourceFile,
   nodeName?: string,
-  nodeType?: "method" | "class" | "interface" | "type" | "function"
-): ts.MethodDeclaration | ts.ClassDeclaration | ts.InterfaceDeclaration | ts.TypeAliasDeclaration | undefined {
-  let foundNode;
+  nodeType?: "method" | "class" | "interface" | "type" | "function",
+): DeclarationNodeType | undefined {
+  let foundNode: DeclarationNodeType | undefined;
   const rootNodeFullText = rootNode.getFullText();
   function _findDeclarations(node: ts.Node) {
     if (
-      (
-        ((ts.isMethodDeclaration(node) || ts.isFunctionDeclaration(node)) && (!nodeType || nodeType === "method" || nodeType === "function")) ||
+      (((ts.isMethodDeclaration(node) || ts.isFunctionDeclaration(node)) &&
+        (!nodeType || nodeType === "method" || nodeType === "function")) ||
         (ts.isClassDeclaration(node) && (!nodeType || nodeType === "class")) ||
-        (ts.isInterfaceDeclaration(node) && (!nodeType || nodeType === "interface")) ||
-        (ts.isTypeAliasDeclaration(node) && (!nodeType || nodeType === "type"))
-      ) && (!nodeName || (nodeName === node.name.getText()))
+        (ts.isInterfaceDeclaration(node) &&
+          (!nodeType || nodeType === "interface")) ||
+        (ts.isTypeAliasDeclaration(node) &&
+          (!nodeType || nodeType === "type"))) &&
+      (!nodeName || nodeName === node.name?.getText())
     ) {
       if (
-        !rootNodeFullText.slice(node?.getStart(rootNode, true), node?.getEnd()).startsWith("/*") &&
-        !rootNodeFullText.slice(node?.getStart(rootNode, true), node?.getEnd()).startsWith("//") &&
-        !node.name.getText().startsWith("lc_") &&
-        !node.name.getText().startsWith("_") &&
-        node.name.getText() !== "serialize" &&
-        node.name.getText() !== "deserialize"
+        !rootNodeFullText
+          .slice(node?.getStart(rootNode, true), node?.getEnd())
+          .startsWith("/*") &&
+        !rootNodeFullText
+          .slice(node?.getStart(rootNode, true), node?.getEnd())
+          .startsWith("//") &&
+        !node.name?.getText().startsWith("lc_") &&
+        !node.name?.getText().startsWith("_") &&
+        node.name?.getText() !== "serialize" &&
+        node.name?.getText() !== "deserialize"
       ) {
         foundNode = node;
       }
@@ -63,46 +83,70 @@ function findMatchingUncommentedDeclarationNode(
     if (!foundNode) {
       ts.forEachChild(node, _findDeclarations);
     }
-  };
+  }
   _findDeclarations(rootNode);
   return foundNode;
 }
 
 for (const filePath of filePaths) {
-  console.log("Adding comments for", filePath)
+  console.log("Adding comments for", filePath);
   const chain = await createGenerateCommentsChain();
   const rawCode = fs.readFileSync(filePath).toString();
   const result = await chain.invoke({
-    input: rawCode
+    input: rawCode,
   });
   const comments = result.ts_doc_comments;
   let updatedCode = rawCode;
   for (const comment of comments) {
-    const sourceNode = ts.createSourceFile("dummy.ts", updatedCode, ts.ScriptTarget.Latest, true);
+    const sourceNode = ts.createSourceFile(
+      "dummy.ts",
+      updatedCode,
+      ts.ScriptTarget.Latest,
+      true,
+    );
     console.log("Searching for", comment.name, "in", filePath);
-    const declarationNode = findMatchingUncommentedDeclarationNode(sourceNode, comment.name, comment.type);
+    const declarationNode = findMatchingUncommentedDeclarationNode(
+      sourceNode,
+      comment.name,
+      comment.type,
+    );
     if (declarationNode) {
-      console.log("Splicing comment for", declarationNode.name.getText(), "in", filePath);
-      const commentLines = comment.text.split(" ").reduce((lines: string[], word) => {
-        if (!lines.length) {
-          lines.push(word);
-        } else {
-          if (lines[lines.length - 1].length + word.length > 70) {
+      console.log(
+        "Splicing comment for",
+        declarationNode.name?.getText(),
+        "in",
+        filePath,
+      );
+      const commentLines = comment.text
+        .split(" ")
+        .reduce((lines: string[], word: string) => {
+          if (!lines.length) {
             lines.push(word);
           } else {
-            lines[lines.length - 1] = lines[lines.length - 1] + " " + word;
+            if (lines[lines.length - 1].length + word.length > 70) {
+              lines.push(word);
+            } else {
+              lines[lines.length - 1] = lines[lines.length - 1] + " " + word;
+            }
           }
-        }
-        return lines;
-      }, []);
-      const extraLines = comment.type === "method" ? comment.params.map((param) => {
-        return `@param ${param.name} ${param.description}`;
-      }) : [];
+          return lines;
+        }, []);
+      const extraLines =
+        comment.type === "method"
+          ? comment.params.map((param: any) => {
+              return `@param ${param.name} ${param.description}`;
+            })
+          : [];
       if (comment.returns && comment.type === "method") {
         extraLines.push(`@returns ${comment.returns}`);
       }
-      const commentString = ["/**", ...commentLines, ...extraLines].join("\n * ") + "\n */";
-      updatedCode = updatedCode.slice(0, declarationNode.getStart()) + commentString + "\n" + updatedCode.slice(declarationNode.getStart())
+      const commentString =
+        ["/**", ...commentLines, ...extraLines].join("\n * ") + "\n */";
+      updatedCode =
+        updatedCode.slice(0, declarationNode.getStart()) +
+        commentString +
+        "\n" +
+        updatedCode.slice(declarationNode.getStart());
     }
   }
   fs.writeFileSync(filePath, updatedCode);
